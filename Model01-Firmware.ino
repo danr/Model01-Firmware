4833d7dc5b3d43aa7a2fdcf85ea82bf02b61821f
Dan Rosén <dan@danrosen.se>
2018-05-05 14:58:12 +0300

    Fix the layout

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -131,18 +131,18 @@ enum { QWERTY, NUMPAD, FUNCTION }; // layers
 KEYMAPS(
 
   [QWERTY] = KEYMAP_STACKED
-  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
-   Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
+  (Key_Backtick,  Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
+   Key_Tab,       Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Minus,
+   Key_CapsLock,  Key_A, Key_S, Key_D, Key_F, Key_G,
+   Key_LeftShift, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_LeftAlt,
+   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_Delete,
    ShiftToLayer(FUNCTION),
 
-   M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
-   Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
-                  Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
+   M(MACRO_ANY),     Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_RightBracket,
+   Key_Equals,       Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_LeftBracket,
+                     Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
+   Key_RightAlt,     Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_RightShift,
+   Key_Backslash,        Key_Enter, Key_Spacebar, Key_RightAlt,
    ShiftToLayer(FUNCTION)),
 
 
@@ -162,21 +162,21 @@ KEYMAPS(
    ___),
 
   [FUNCTION] =  KEYMAP_STACKED
-  (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           XXX,
-   Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
-   Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
-   Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
-   ___, Key_Delete, ___, ___,
+  (___, Key_F1,           Key_F2,      Key_F3,     Key_F4,           Key_F5,           XXX,
+   ___, Key_Home,      Key_PageDown,  Key_PageUp,   Key_End,         ___,    ___,
+   ___, Key_LeftArrow, Key_DownArrow, Key_UpArrow,  Key_RightArrow,  ___,
+   ___, Key_mouseL,    Key_mouseDn,   Key_mouseUp,  Key_mouseR,      ___,    Key_Enter,
+   Key_mouseBtnM,  Key_mouseBtnL, Key_mouseBtnR, ___,
    ___,
 
-   Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
-   Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
-                               Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
-   Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
-   ___, ___, Key_Enter, ___,
+   ___,       Key_F6,   Key_F7,        Key_F8,        Key_F9,       Key_F10, Key_F11,
+   ___,       ___,      Key_Home,      Key_PageDown,  Key_PageUp,   Key_End,         Key_F12,
+              ___,      Key_LeftArrow, Key_DownArrow, Key_UpArrow,  Key_RightArrow,  ___,
+   Key_Enter, ___,      Key_mouseL,    Key_mouseDn,   Key_mouseUp,  Key_mouseR,      ___,
+   ___, Key_mouseBtnM,  Key_mouseBtnL, Key_mouseBtnR,
    ___)
 
-	) // KEYMAPS(
+    ) // KEYMAPS(
 
 /* Re-enable astyle's indent enforcement */
 // *INDENT-ON*
@@ -306,28 +306,28 @@ void setup() {
 
     // The rainbow effect changes the color of all of the keyboard's keys at the same time
     // running through all the colors of the rainbow.
-    &LEDRainbowEffect,
+    // &LEDRainbowEffect,
 
     // The rainbow wave effect lights up your keyboard with all the colors of a rainbow
     // and slowly moves the rainbow across your keyboard
-    &LEDRainbowWaveEffect,
+    // &LEDRainbowWaveEffect,
 
     // The chase effect follows the adventure of a blue pixel which chases a red pixel across
     // your keyboard. Spoiler: the blue pixel never catches the red pixel
-    &LEDChaseEffect,
+    // &LEDChaseEffect,
 
     // These static effects turn your keyboard's LEDs a variety of colors
-    &solidRed, &solidOrange, &solidYellow, &solidGreen, &solidBlue, &solidIndigo, &solidViolet,
+    // &solidRed, &solidOrange, &solidYellow, &solidGreen, &solidBlue, &solidIndigo, &solidViolet,
 
     // The breathe effect slowly pulses all of the LEDs on your keyboard
-    &LEDBreatheEffect,
+    // &LEDBreatheEffect,
 
     // The AlphaSquare effect prints each character you type, using your
     // keyboard's LEDs as a display
-    &AlphaSquareEffect,
+    // &AlphaSquareEffect,
 
     // The stalker effect lights up the keys you've pressed recently
-    &StalkerEffect,
+    // &StalkerEffect,
 
     // The numpad plugin is responsible for lighting up the 'numpad' mode
     // with a custom LED effect
